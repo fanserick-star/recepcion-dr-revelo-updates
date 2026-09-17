5e31c1af7a4f9370b893b98fe53642b7d6baf787
fanserick-star <fanserick@hotmail.com>
2026-09-17 16:47:37 -0500

    v4.4.81 app entrypoint

---
+++ updates/v4_4_81_remove_facturero_modal/app.py
@@ -0,0 +1,13 @@
+from app_patch_4481 import *  # noqa: F401,F403
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=core.LOCAL_HTTP_PORT,
+        reload=False,
+        access_log=False,
+        log_level="warning",
+        workers=1,
+    )
